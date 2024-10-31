5e68da20af42245531933732dbd52fab93d20342
Huỳnh Quân <huynhquan19111995@gmail.com>
2024-10-31 09:00:29 +0700

    update

--- SV21T1020620.Web/Controllers/CustomerController.cs
+++ SV21T1020620.Web/Controllers/CustomerController.cs
@@ -1,27 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SV21T1020620.BusinessLayers;
 using SV21T1020620.DomainModels;
+using SV21T1020620.Web.Models;
 
 namespace SV21T1020620.Web.Controllers
 {
     public class CustomerController : Controller
     {
-        private const int PAGE_SIZE = 20;
-        public IActionResult Index(int page = 1, string searchValue = "")
+        private const int PAGE_SIZE = 30;
+        private const string CUSTOMER_SEARCH_CONDITION = "CustomerSearchCondition";
+        public IActionResult Index()
+        {
+            PaginationSearchInput? condition = ApplicationContext.GetSessionData<PaginationSearchInput>(CUSTOMER_SEARCH_CONDITION);
+            if (condition == null)
+                condition = new PaginationSearchInput()
+                {
+                    Page = 1,
+                    PageSize = PAGE_SIZE,
+                    SearchValue = ""
+                };
+            return View(condition);
+
+        }
+
+        public IActionResult Search(PaginationSearchInput condition) 
         {
             int rowCount;
-            var data = CommonDataService.ListOfCustomers(out rowCount, page, PAGE_SIZE, searchValue ?? "");
-
-            int pageCount = rowCount / PAGE_SIZE;
-            if (rowCount % PAGE_SIZE > 0)
-                pageCount += 1;
-
-            ViewBag.Page = page;
-            ViewBag.RowCount = rowCount;
-            ViewBag.PageCount = pageCount;
-            ViewBag.SearchValue = searchValue;
-
-            return View(data);      //List<Customer>  IEnumerable<Customer>
+            var data = CommonDataService.ListOfCustomers(out rowCount, condition.Page, condition.PageSize, condition.SearchValue ?? "");
+            CustomerSearchResult model = new CustomerSearchResult()
+            {
+                Page = condition.Page,
+                PageSize = condition.PageSize,
+                SearchValue = condition.SearchValue ?? "",
+                RowCount = rowCount,
+                Data = data
+            };
+            ApplicationContext.SetSessionData(CUSTOMER_SEARCH_CONDITION, condition);
+            return View(model);
         }
         public IActionResult Create()
         {
